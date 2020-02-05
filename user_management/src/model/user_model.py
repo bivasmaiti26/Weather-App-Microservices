6a0e570af230bba007831cf4e4de331aeb068341
dshinde <dshinde@iu.edu>
2020-02-05 15:30:28 -0500

    First draft: User entity model

---
+++ user_management/src/model/user_model.py
@@ -0,0 +1,20 @@
+# user_management/src/model/user_model.py
+
+from user_management.src import db
+import datetime
+
+class User(db.Model):
+    """ User Model for storing user related details """
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, email, password, admin=False):
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
