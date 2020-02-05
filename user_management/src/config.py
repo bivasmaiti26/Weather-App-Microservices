3cf085c9d9bd41cf4533ce40d791087c7c2f747f
dshinde <dshinde@iu.edu>
2020-02-05 15:55:42 -0500

    Added configuration regarding Bcrypt log rounds

--- user_management/src/config.py
+++ user_management/src/config.py
@@ -7,11 +7,13 @@ class BaseConfig:
     """Base configuration."""
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
     DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration."""
     DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
     SQLALCHEMY_DATABASE_URI = constant.DATABASE_BASE_URL + constant.DATABASE_NAME
     
