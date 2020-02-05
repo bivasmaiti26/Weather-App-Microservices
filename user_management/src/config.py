5fa7afb5dbd9a69c1d437c698fa1b3403dc727df
dshinde <dshinde@iu.edu>
2020-02-05 15:22:54 -0500

    Change in default secret key

--- user_management/src/config.py
+++ user_management/src/config.py
@@ -5,7 +5,7 @@ from user_management.src.const import Constant as constant
 
 class BaseConfig:
     """Base configuration."""
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
