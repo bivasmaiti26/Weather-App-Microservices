1260920fa460ff0e47080d69fedb72373c431084
dshinde <dshinde@iu.edu>
2020-02-05 16:25:24 -0500

    Added methods for creating and validating JWT token

--- user_management/src/model/user_model.py
+++ user_management/src/model/user_model.py
@@ -1,6 +1,8 @@
 # user_management/src/model/user_model.py
 
-from user_management.src import app, db, bcrypt
+from user_management.src import app, db, bcrypt, const
+
+import jwt
 import datetime
 
 class User(db.Model):
@@ -8,12 +10,14 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
 
-    def __init__(self, email, password, admin=False):
+    def __init__(self, username, email, password, admin=False):
+        self.username = username
         self.email = email
         self.password = bcrypt.generate_password_hash(
             password, app.config.get('BCRYPT_LOG_ROUNDS')
@@ -22,4 +26,36 @@ class User(db.Model):
         self.admin = admin
         
     def __repr__(self):
-        return '<User %r>' % self.email
+        return '<User %r>' % self.username
+    
+    def generate_token(self):
+        """
+        Generates the JWT Token
+        :return: string
+        """
+        try:
+            exp_time = const.Constant.JWT_TOKEN_EXP_DAY_HR_MIN_SEC
+            exp_day, exp_hr, exp_min, exp_sec = exp_time.split(':')
+            payload = {
+                    'sub': self.username,
+                    'admin':self.admin,
+                    'iat': datetime.datetime.utcnow(),
+                    'exp': datetime.datetime.utcnow() + 
+                           datetime.timedelta(days=int(exp_day), hours=int(exp_hr), minutes=int(exp_min), seconds=int(exp_sec))
+                    }
+            return jwt.encode(payload,
+                              app.config.get('SECRET_KEY'),
+                              algorithm='HS256'
+                              )
+        except Exception as e:
+            return e
+    
+    @staticmethod    
+    def validate_token(token):
+        try:
+            payload = jwt.decode(token, app.config.get('SECRET_KEY'))
+            return payload['sub'], payload['admin']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
