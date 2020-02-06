c686e01875f54877bb370b9073cb5c72d11bef48
dshinde <dshinde@iu.edu>
2020-02-06 00:46:54 -0500

    Revert "updated user registration api"
    This reverts commit c34dfcfb6757012ff768121a97bbd10508447d7a.

--- user_management/src/api/register_api.py
+++ user_management/src/api/register_api.py
@@ -1,53 +1,64 @@
 # user_management/scr/api/register_api.py
 
+from flask.views import MethodView
 from flask import request, make_response, jsonify, Blueprint
 
 from user_management.src import db
 from user_management.src.model.user_model import User
 
-register_api_blueprint = Blueprint('auth', __name__, url_prefix='/auth')
+register_api_blueprint = Blueprint('api', __name__)
 
-"""
-User registration api
-"""
-@register_api_blueprint.route('/register', method=('POST'))
-def register():
-    data = request.get_json()
+class RegisterAPI(MethodView):
+    """
+    User registration api
+    """
     
-    user = User.query.filter_by(username=data.get('username')).first()
-    
-    if not user:
-        try:
-            user = User(
-                    username=data.get('username'),
-                    email=data.get('email'),
-                    password=data.get('password')
-                    )
+    def post(self):
+        data = request.get_json()
+        
+        user = User.query.filter_by(username=data.get('username')).first()
+        
+        if not user:
+            try:
+                user = User(
+                        username=data.get('username'),
+                        email=data.get('email'),
+                        password=data.get('password')
+                        )
+                
+                # insert user in database
+                db.session.add(user)
+                db.session.commit()
+                
+                token = user.generate_token()
+                
+                response = {
+                        'status': 'success',
+                        'message': 'User successfully registered.',
+                        'auth_token': token.decode()
+                        }
+                return make_response(jsonify(response)), 201
             
-            # insert user in database
-            db.session.add(user)
-            db.session.commit()
+            except Exception:
+                response = {
+                        'status': 'fail',
+                        'message': 'Some error occurred. Please try again.',
+                        }
+                return make_response(jsonify(response)), 401
             
-            token = user.generate_token()
-            
+        else:
             response = {
-                    'status': 'success',
-                    'message': 'User successfully registered.',
-                    'auth_token': token.decode()
-                    }
-            return make_response(jsonify(response)), 201
+                        'status': 'fail',
+                        'message': 'User already exists. Please log in.',
+                        }
+            return make_response(jsonify(response)), 202
         
-        except Exception:
-            response = {
-                    'status': 'fail',
-                    'message': 'Some error occurred. Please try again.',
-                    }
-            return make_response(jsonify(response)), 401
-        
-    else:
-        response = {
-                    'status': 'fail',
-                    'message': 'User already exists. Please log in.',
-                    }
-        return make_response(jsonify(response)), 202
-        
+# Register API with view
+register_view = RegisterAPI.as_view('register_api')
+
+# Route path for API endpoint
+register_api_blueprint.add_url_rule(
+    '/api/register',
+    view_func=register_view,
+    methods=['POST']
+)
