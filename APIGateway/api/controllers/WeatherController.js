48153ff1ca55a604fbaf3ada608288ace8570820
Virendra Wali <vwali@iu.edu>
2020-02-07 18:55:52 -0500

    service discovery function
    function to retrieve host and port number of microservice

---
+++ APIGateway/api/controllers/WeatherController.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+exports.getWeatherData = function() {
+    var zookeeper = require('node-zookeeper-client');
+    var host,port;
+    var client = zookeeper.createClient('localhost:2181');
+    var path = '/WeatherData';
+    var url;
+    client.getData(
+        path,
+        function (event) {
+            console.log('Got event: %s.', event);
+        },
+        function (error, data, stat) {
+            if (error) {
+                console.log(error.stack);
+                return;
+            }
+            zookeeper_data = JSON.parse(data.toString('utf8'))
+            host = zookeeper_data["host"];
+            port = zookeeper_data["port"];
+            url = "http://"+host+":"+port.toString()+'/getWeatherData';
+        }
+    );    
+    client.connect();
+};
