8eeb3224c654efdff263fe3fae8929dc4f2ecbd2
dshinde <dshinde@iu.edu>
2020-02-09 16:52:46 -0500

    Added a new constant APP_SETTINGS_TEST for test environment

--- user_management/src/const.py
+++ user_management/src/const.py
@@ -4,6 +4,7 @@ class Constant:
     DATABASE_BASE_URL = 'postgresql://postgres:1234@localhost/'
     DATABASE_NAME = 'user_management'
     APP_SETTINGS = 'user_management.src.config.DevelopmentConfig'
+    APP_SETTINGS_TEST = 'user_management.src.config.TestConfig'
     JWT_TOKEN_EXP_DAY_HR_MIN_SEC = '00:00:00:05'
     
     
