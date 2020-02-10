61ae65f433ca77e9c1ed5ed31d39128d7a5fbf37
Virendra Wali <vwali@iu.edu>
2020-02-10 18:15:44 -0500

    added cors dependency

--- APIGateway/server.js
+++ APIGateway/server.js
@@ -3,8 +3,11 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   bodyParser = require('body-parser');
 
+var cors = require('cors');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var routes = require('./api/routes/routes'); //importing route
 routes(app); //register the route
