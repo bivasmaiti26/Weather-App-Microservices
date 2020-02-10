cf5e6b71ef684b694ba6a4bf11e1b7582260bb6d
dshinde <dshinde@iu.edu>
2020-02-09 22:27:56 -0500

    Added routing information for register, login and user_details

--- APIGateway/api/routes/routes.js
+++ APIGateway/api/routes/routes.js
@@ -1,8 +1,18 @@
 'use strict';
 module.exports = function(app) {
   var WeatherControllerObject = require('../controllers/WeatherController');
-
+  var UserManagementControllerObject = require('../controllers/UserManagementController');
+  
   // todoList Routes
   app.route('/getWeatherData')
-    .get(WeatherControllerObject.getWeatherData)   
+    .get(WeatherControllerObject.getWeatherData);
+    
+  app.route('/login')
+    .post(UserManagementControllerObject.login);
+
+  app.route('/register')
+    .post(UserManagementControllerObject.register); 
+    
+    app.route('/user_details')
+    .get(UserManagementControllerObject.user_details); 
 };
