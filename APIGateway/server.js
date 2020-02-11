d708db30cfbf29c4ff691c5c6c131c4ca820ed14
dshinde <dshinde@iu.edu>
2020-02-10 22:33:13 -0500

    Changed default port for running APIGateway microservice

--- APIGateway/server.js
+++ APIGateway/server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 8000,
   bodyParser = require('body-parser');
 
 var cors = require('cors');
