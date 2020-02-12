7168820fe1e29876ee42fb3dce5aed91dc063a73
Virendra Wali <vwali@iu.edu>
2020-02-12 18:12:09 -0500

    removed unneccesary imports

--- UI/src/Landingpage.js
+++ UI/src/Landingpage.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import ls from "local-storage";
-import ReactTypingEffect from 'react-typing-effect';
 import  "./App"
 
 
