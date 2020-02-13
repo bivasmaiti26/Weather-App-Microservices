58813e1075f4be0d411ff980196b47cfc9a49117
Virendra Wali <vwali@iu.edu>
2020-02-12 20:39:19 -0500

    removed unwanted code

--- UI/src/Dashboard.js
+++ UI/src/Dashboard.js
@@ -3,46 +3,30 @@ import axios from "axios";
 import Card from "@material-ui/core/Card";
 import Sidebar from './Sidebar'
 import Search from './Search'
+import WeatherData from "./weather_data";
+import { Table } from "@material-ui/core";
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      withdraw: 0 ,
-      balance: 0,
-      credilimit:0 ,      
+    this.state = {      
     };
-    this.getData = this.getData.bind(this);
   }
-
-  getData() {
-    return axios({
-      method: "post",
-      url: "http://127.0.0.1:5000/ getData",
-      headers: { "Access-Control-Allow-Origin": "*" },
-      data: {'email':localStorage.getItem('currentUser')}
-    })
-      .then(response => {
-        this.setState({withdraw: response.data['withdraw']})
-        this.setState({balance: response.data['balance']})
-        this.setState({credilimit:response.data['creditlimit'] })
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
 
   render() {
       return(
           <>
           <meta http-equiv="Content-Security-Policy" content="default-src 'self'"/>
-            <div className="d-flex" id="wrapper">
+            <div className="d-flex">
             <Sidebar/>
-            <div id="page-content-wrapper">
-            
-            </div>
+            <div style={{display:Table}}>
+            <div>
             <Search/>
+            </div>
+            <div style={{paddingLeft:50}}>
+            <WeatherData/>
+            </div>
+            </div>
             </div>        
  </>
       );
