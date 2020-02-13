b861bc8db7404f902cc5b3f7d671a1c60d6b79f5
dshinde <dshinde@iu.edu>
2020-02-13 16:13:27 -0500

    Changed JWT token expiry time

--- user_management/src/const.py
+++ user_management/src/const.py
@@ -5,7 +5,7 @@ class Constant:
     DATABASE_NAME = 'user_management'
     APP_SETTINGS = 'src.config.DevelopmentConfig'
     APP_SETTINGS_TEST = 'src.config.TestConfig'
-    JWT_TOKEN_EXP_DAY_HR_MIN_SEC = '00:00:00:05'
+    JWT_TOKEN_EXP_DAY_HR_MIN_SEC = '00:00:30:00'
     HOST = '127.10.10.7'
     PORT = '3000'
     
