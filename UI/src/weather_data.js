3c516a4ded81f3948c28b1ae1578f6c04b4cc016
Virendra Wali <vwali@iu.edu>
2020-02-12 20:40:19 -0500

    component to read weather data using socket

---
+++ UI/src/weather_data.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { set } from "local-storage";
+
+class WeatherData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherData: ""     
+    };  
+    this.setdata = this.setdata.bind(this);
+}
+
+  setdata = (evt) => {
+    this.setState({weatherData:JSON.parse(evt.data)}) 
+  }
+
+  componentDidMount()
+  {
+    var myWebSocket = new WebSocket("ws://localhost:9090");
+            
+        //this.setState({weatherData:evt.data})
+    myWebSocket.onmessage = this.setdata;
+  }
+
+
+  render() {
+      if(this.state.weatherData!=="")
+      {
+          const tableData = this.state.weatherData.map((jsonData) =>
+            <tr>
+                <td style={{width:230}}>{jsonData.startTime}</td>
+                <td style={{width:230}}>{jsonData.endTime}</td>
+                <td style={{width:30}}>{jsonData.temperature}</td>
+                <td style={{width:50}}>{jsonData.temperatureUnit}</td>
+                <td style={{width:70}}>{jsonData.windSpeed}</td>
+                <td style={{width:30}}>{jsonData.windDirection}</td>
+                <td style={{width:230}}>{jsonData.shortForecast}</td>
+            </tr>
+          );
+      return(
+          <>
+          <table>
+          {tableData}
+          </table> 
+    </>
+      );
+      }
+      else
+      {
+        return(
+            <>
+            
+                    
+      </>
+        );   
+      }
+    } 
+}
+
+export default WeatherData;
