dae6378a48c98e1c00d85e1e67b4f7e8d49db446
Bivas Maiti <bivas-mac@149-160-170-250.dhcp-bl.indiana.edu>
2020-02-13 20:10:26 -0500

    changed host and port in user management

--- user_management/src/const.py
+++ user_management/src/const.py
@@ -6,8 +6,8 @@ class Constant:
     APP_SETTINGS = 'src.config.DevelopmentConfig'
     APP_SETTINGS_TEST = 'src.config.TestConfig'
     JWT_TOKEN_EXP_DAY_HR_MIN_SEC = '00:00:00:05'
-    HOST = '127.10.10.7'
-    PORT = '3000'
+    HOST = 'localhost'
+    PORT = '6000'
     
     
     
