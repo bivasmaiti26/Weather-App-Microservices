8839eef617d7961e91fc1a84c13a87acbbd4aa91
Bivas Maiti <bivas-mac@149-160-170-250.dhcp-bl.indiana.edu>
2020-02-13 20:06:18 -0500

    Added new controller to save session

--- sessionmanagement/src/main/java/com/blitzkrieg/sessionmanagement/controllers/SessionController.java
+++ sessionmanagement/src/main/java/com/blitzkrieg/sessionmanagement/controllers/SessionController.java
@@ -1,18 +1,16 @@
 package com.blitzkrieg.sessionmanagement.controllers;
 import com.blitzkrieg.sessionmanagement.models.Session;
 import com.blitzkrieg.sessionmanagement.repository.SessionRepository;
+import com.blitzkrieg.sessionmanagement.security.TokenUtility;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController("/")
-public class TokenController {
+public class SessionController {
     private final SessionRepository repository ;
-    public TokenController(SessionRepository repository) {
+    public SessionController(SessionRepository repository) {
         this.repository = repository;
     }
     @GetMapping("/getSessions/{username}")
@@ -21,7 +19,11 @@ public class TokenController {
         return repository.findByUserName(username);
     }
     @PostMapping("/addNewSession")
-    public saveSession(Session session,);
+    public Session saveSession(@RequestBody Session session){
+        TokenUtility util = new TokenUtility();
+        session.setUserName(util.getUserNameFromToken(session.getToken()));
+        return repository.save(session);
+    }
 
 
 
