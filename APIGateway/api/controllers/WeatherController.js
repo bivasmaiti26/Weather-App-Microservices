3553db89fd95dca5460a939ff78f369b25ba5513
Bivas Maiti <bivas-mac@149-160-170-250.dhcp-bl.indiana.edu>
2020-02-13 20:01:12 -0500

    Added Session for axios call

--- APIGateway/api/controllers/WeatherController.js
+++ APIGateway/api/controllers/WeatherController.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-
+const utils = require('../utils');
 exports.getWeatherData = function(req,res) {
     var zookeeper = require('node-zookeeper-client');
     var host,port;
@@ -19,15 +19,24 @@ exports.getWeatherData = function(req,res) {
             zookeeper_data = JSON.parse(data.toString('utf8'))
             host = zookeeper_data["host"];
             port = zookeeper_data["port"];
-            var long = req.body.lng;
-            var lat = req.body.lat;
+            let token = '';
+            auth_header = req.header('Authorization');
+            console.log(auth_header);
+            
+            if (auth_header && auth_header.split(' ')[0] === 'Bearer') {
+                token =  auth_header.split(' ')[1];
+            }
+            console.log()
+            var long = req.body.latlng.lng;
+            var lat = req.body.latlng.lat;
+            var city = req.body.value;
             url = "http://"+host+":"+port.toString()+'/model-executor?lat=' + lat + '&long=' + long;
-            WeatherServiceAPICall(url,req.body,res)
+            WeatherServiceAPICall(url,res,token,city)
         }
     );    
     client.connect();
 
-    function WeatherServiceAPICall(url,req,res)
+    function WeatherServiceAPICall(url,res,token,cityName)
     {
         return axios({
             method: "post",
@@ -38,9 +47,23 @@ exports.getWeatherData = function(req,res) {
           })
           .then(response => {
             console.log("Data added to kafka")
+            utils.addNewSession({
+              requestTime:new Date(),
+              requestName: "Weather",
+              city: cityName,
+              requestStatus:true,
+              token, token
+            });
             res.send('')
           })
           .catch(err => {
+            utils.addNewSession({
+              requestTime:new Date(),
+              token, token,
+              requestName: "Weather",
+              city: cityName,
+              requestStatus:false
+            });
             console.log(err);
           });
     }
