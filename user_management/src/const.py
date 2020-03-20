989775bb0fc3182da356775b6ea8e87112bc9ad1
Darshan Shinde <darshinde7802@gmail.com>
2020-03-20 17:09:26 -0400

    Update const.py

--- user_management/src/const.py
+++ user_management/src/const.py
@@ -1,7 +1,7 @@
 # user_management/src/Const.py
 
 class Constant:
-    DATABASE_BASE_URL = 'postgresql://docker:docker@localhost/'
+    DATABASE_BASE_URL = 'postgresql://docker:docker@149.165.171.151:30543/'
     DATABASE_NAME = 'user_management'
     APP_SETTINGS = 'src.config.DevelopmentConfig'
     APP_SETTINGS_TEST = 'src.config.TestConfig'
