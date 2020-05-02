511d56600416ecdfc4b52988f5b3931ba813260a
Bivas Maiti <bivasmaiti26@gmail.com>
2020-05-01 20:21:03 -0400

    Update Header.js

--- UI/src/Header.js
+++ UI/src/Header.js
@@ -144,7 +144,7 @@ class Header extends Component {
 
       return axios({
           method: 'post',
-          url: 'http://149.165.170.68:30008/register',
+          url: 'http://149.165.170.68:30038/register',
           headers: {
             'Access-Control-Allow-Origin': '*'
           },
@@ -166,7 +166,7 @@ class Header extends Component {
     sendEmail(user) {
       return axios({
           method: 'post',
-          url: 'http://149.165.170.68:30008/welcomeEmail',
+          url: 'http://149.165.170.68:30038/welcomeEmail',
           headers: {
             'Access-Control-Allow-Origin': '*'
           },
