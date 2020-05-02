965f19de73239afd069150f2fcd87219578181e5
Bivas Maiti <bivasmaiti26@gmail.com>
2020-05-01 20:24:29 -0400

    Update weather_data.js

--- UI/src/weather_data.js
+++ UI/src/weather_data.js
@@ -16,7 +16,7 @@ class WeatherData extends Component {
 
   componentDidMount()
   {
-    var myWebSocket = new WebSocket("ws://149.165.170.68:30010");
+    var myWebSocket = new WebSocket("ws://149.165.170.68:30031");
             
         //this.setState({weatherData:evt.data})
     myWebSocket.onmessage = this.setdata;
